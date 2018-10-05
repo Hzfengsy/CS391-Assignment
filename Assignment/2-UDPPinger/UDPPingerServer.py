3b3110f20dfa92d6d712b30b1a7057511db9c720
Hzfengsy <Hzfengsy@vip.qq.com>
2018-10-05 13:43:25 +0800

    UDP heartbeat

--- Assignment/2-UDPPinger/UDPPingerServer.py
+++ Assignment/2-UDPPinger/UDPPingerServer.py
@@ -1,22 +1,70 @@
 # We will need the following module to generate randomized lost packets
 import random
 import socket
+import threading
+import time
 # Create a UDP socket
 # Notice the use of SOCK_DGRAM for UDP packets
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Assign IP address and port number to socket
 serverSocket.bind(('0.0.0.0', 8080))
-while True:
-    # Generate random number in the range of 0 to 10
-    rand = random.randint(0, 10)
-    # Receive the client packet along with the address it is coming from
-    message, address = serverSocket.recvfrom(1024)
-    # Capitalize the message from the client
-    message = message.upper()
-    # If rand is less is than 4, we consider the packet lost and do not respond
-    if rand < 4:
-        continue
-    # Otherwise, the server responds
-    serverSocket.sendto(message, address)
 
-serverSocket.close()
+clients = {}
+lock = threading.Lock()
+timeout = 5.0
+delay = 1.0 
+
+def recvMsg():
+    def messageDecode(message):
+        _, index, timeStamp = message.split(' ', 2)
+        return int(index), float(timeStamp)
+
+    global clients
+    try:
+        while True:
+            # Generate random number in the range of 0 to 10
+            rand = random.randint(0, 10)
+            # Receive the client packet along with the address it is coming from
+            message, address = serverSocket.recvfrom(1024)
+            index, timeStamp = messageDecode(message.decode())
+            lock.acquire()
+            if address in clients:
+                clients[address]['total'] += index - clients[address]['last']
+                clients[address]['loss']  += index - clients[address]['last'] - 1
+                clients[address]['last']  = index
+                clients[address]['time']  = timeStamp
+            else:
+                print('client %s connected' % (str(address)))
+                clients[address] = {'total': 1, 'loss': 0, 'last': index, 'time': timeStamp}
+            lock.release()
+            # Capitalize the message from the client
+            message = message.upper()
+            # If rand is less is than 4, we consider the packet lost and do not respond
+            if rand < 4:
+                continue
+            # Otherwise, the server responds
+            serverSocket.sendto(message, address)
+    except: 
+        pass
+
+thread = threading.Thread(target=recvMsg)
+thread.start()
+try:
+    while True:
+        lock.acquire()
+        for client in list(clients.keys()):
+            info = clients[client]
+            if time.time() - info['time'] > timeout:
+                total, loss = info['total'], info['loss']
+                print('client %s heartbeat timeout' % (str(client)))
+                print("%d packets transmitted, %d packets received, %.1f%% packet loss" % (total, total - loss, loss / total * 100))
+                clients.pop(client)
+        lock.release()
+        time.sleep(delay)
+except KeyboardInterrupt:
+    pass
+finally:
+    serverSocket.close()
+
+
+
--- Assignment/2-UDPPinger/client.py
+++ Assignment/2-UDPPinger/client.py
@@ -1,20 +1,27 @@
 import socket 
 import time
 import numpy as np
+import threading
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 addr = ('localhost', 8080)
 rtts = []
-loss = 0.0
+loss = 0
 total = 10
-for i in range(total):
+sleep_time = 1.0
+UDP_timeout = 1.0
+threads = []
+
+def sendUDP(i):
+    global loss, rtts
     try:
         start_time = time.time()
         sequence_number = str(i + 1)
-        localtime = time.asctime( time.localtime(start_time) )
+        # localtime = time.asctime( time.localtime(start_time) )
+        localtime = str(start_time)
         message = ' '.join(['PING', sequence_number, localtime])
         clientSocket.sendto(message.encode(), addr)
-        clientSocket.settimeout(1.0)
+        clientSocket.settimeout(UDP_timeout)
         recv_message, address = clientSocket.recvfrom(1024)
         recv_message = recv_message.decode()
         end_time = time.time()
@@ -25,7 +32,20 @@ for i in range(total):
     except socket.timeout:
         loss += 1
         print("PING %s: Request timed out." % sequence_number)
+try:
+    for i in range(total):
+        thread = threading.Thread(target=sendUDP, args=(i,))
+        thread.start()
+        threads.append(thread)
+        time.sleep(sleep_time)
 
-print("%d packets transmitted, %d packets received, %.1f%% packet loss" % (total, total - loss, loss / total * 100))
-print("round-trip time min/avg/max = %.3f/%.3f/%.3f ms" % (min(rtts), np.mean(rtts), max(rtts)))
-clientSocket.close()
+except KeyboardInterrupt:
+    pass
+
+finally:
+    for thread in threads:
+        thread.join()
+    print("%d packets transmitted, %d packets received, %.1f%% packet loss" % (total, total - loss, loss / total * 100))
+    if len(rtts) > 0:
+        print("round-trip time min/avg/max = %.3f/%.3f/%.3f ms" % (min(rtts), np.mean(rtts), max(rtts)))
+    clientSocket.close()
